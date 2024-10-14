cfa9e82ceee13b9abac716f33f6e7b2cbca65939
Giulia Moukouyou <giuliadeveloper@users.noreply.github.com>
2024-10-14 02:03:54 +0200

    Update style

--- script.js
+++ script.js
@@ -36,10 +36,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 /*  --------------------------------------------------------------------------------------------
+    GO TO ABOUT SECTION ON CLICK TO THE "ABOUT ME" BUTTON
+    -------------------------------------------------------------------------------------------- */
+    document.addEventListener('DOMContentLoaded', function() {
+        const elements = document.getElementsByClassName('go-to-about-me');
+            for (let i = 0; i < elements.length; i++) {
+            elements[i].addEventListener('click', function() {
+                document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+            });
+        }
+    });
+
+/*  --------------------------------------------------------------------------------------------
+    GO TO PORTFOLIO SECTION ON CLICK TO THE "GO TO PORTFOLIO" BUTTON
+    -------------------------------------------------------------------------------------------- */
+    document.addEventListener('DOMContentLoaded', function() {
+        const elements = document.getElementsByClassName('see-portfolio');
+            for (let i = 0; i < elements.length; i++) {
+            elements[i].addEventListener('click', function() {
+                document.getElementById('portfolio').scrollIntoView({ behavior: 'smooth' });
+            });
+        }
+    });
+
+
+/*  --------------------------------------------------------------------------------------------
     CHANGE THE SOURCE OF IMAGES (TABLET / DESKTOP/ DEFAULT MOBILE) DEPENDING ON ON WINDOW WIDTH:
     --------------------------------------------------------------------------------------------
     HERO IMAGE 
-    ------------ */
+    ------------
 var desktop_image = document.querySelector(".hero-image");
 function replacePattern() {
 
@@ -65,7 +90,7 @@ window.addEventListener('resize', replacePattern);
     
 /*  ------------------------
     ABOUT ME IMAGE 
-    ------------------------ */
+    ------------------------ 
 var profile_image = document.querySelector(".profile-image");
 function replacePortrait() {
 
@@ -90,7 +115,7 @@ window.addEventListener('resize', replacePortrait);
 
 /*  ------------------------
     "MANAGE" PROJECT IMAGE 
-    ------------------------ */
+    ------------------------ 
     var manage_image = document.querySelector(".manage");
     function replaceManage() {
     
@@ -112,9 +137,11 @@ window.addEventListener('resize', replacePortrait);
     replaceManage();
     window.addEventListener('resize', replaceManage);
 
-    /*  ------------------------
+
+    /*  
+    ------------------------
     "INSURE" PROJECT IMAGE 
-    ------------------------ */
+    ------------------------ 
     var insure_image = document.querySelector(".insure");
     function replaceInsure() {
     
@@ -136,9 +163,11 @@ window.addEventListener('resize', replacePortrait);
     replaceInsure();
     window.addEventListener('resize', replaceInsure);
 
-    /*  ------------------------
+
+    /*  
+    ------------------------
     "BOOKMARK" PROJECT IMAGE 
-    ------------------------ */
+    ------------------------ 
     var bookmark_image = document.querySelector(".bookmark");
     function replaceBookmark() {
     
@@ -159,6 +188,132 @@ window.addEventListener('resize', replacePortrait);
     
     replaceBookmark();
     window.addEventListener('resize', replaceBookmark);
+
+    */
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    /*  ----------------------------
+        HERO IMAGE 
+        ---------------------------- */
+    var desktop_image = document.querySelector(".hero-image");
+    function replacePattern() {
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        console.clear();
+        
+        if(windowWidth >= 768 && windowWidth < 1024) {
+            desktop_image.src = "./images/homepage/tablet/image-homepage-hero.jpg";
+            console.log(`hero-image src: ${desktop_image.src}`);
+        }
+        else if (windowWidth >= 1024) {
+            desktop_image.src = "./images/homepage/desktop/image-homepage-hero.jpg"; 
+            console.log(`hero-image src: ${desktop_image.src}`);
+        } else {
+            desktop_image.src = " ./images/homepage/mobile/image-homepage-hero.jpg";
+            console.log(`hero-image src: ${desktop_image.src}`);
+        }
+    }
+
+    replacePattern();
+    window.addEventListener('resize', replacePattern);
+
+
+    /*  ----------------------------
+        ABOUT ME IMAGE 
+        ---------------------------- */
+    var profile_image = document.querySelector(".profile-image");
+    function replacePortrait() {
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        
+        if(windowWidth >= 768 && windowWidth < 1024) {
+            profile_image.src = "./images/homepage/tablet/image-homepage-profile.jpg";
+            console.log(`profile-image src: ${profile_image.src}`);
+        }
+        else if (windowWidth >= 1024) {
+            profile_image.src = "./images/homepage/desktop/image-homepage-profile.jpg"; 
+            console.log(`profile-image src: ${profile_image.src}`);
+        } else {
+            profile_image.src = " ./images/homepage/mobile/image-homepage-profile.jpg";
+            console.log(`profile-image src: ${profile_image.src}`);
+        }
+    }
+
+    replacePortrait();
+    window.addEventListener('resize', replacePortrait);
+
+
+    /*  ----------------------------
+        "MANAGE" PROJECT IMAGE 
+        ---------------------------- */
+    var manage_image = document.querySelector(".manage");
+    function replaceManage() {
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        
+        if(windowWidth >= 768 && windowWidth < 1024) {
+            manage_image.src = "./images/portfolio/tablet/image-portfolio-manage.jpg";
+            console.log(`manage-image src: ${manage_image.src}`);
+        }
+        else if (windowWidth >= 1024) {
+            manage_image.src = "./images/portfolio/desktop/image-portfolio-manage.jpg"; 
+            console.log(`manage-image src: ${manage_image.src}`);
+        } else {
+            manage_image.src = " ./images/portfolio/mobile/image-portfolio-manage.jpg";
+            console.log(`manage-image src: ${manage_image.src}`);
+        }
+    }
+
+    replaceManage();
+    window.addEventListener('resize', replaceManage);
+
+
+    /*  ----------------------------
+        "INSURE" PROJECT IMAGE 
+        ---------------------------- */
+    var insure_image = document.querySelector(".insure");
+    function replaceInsure() {
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        
+        if(windowWidth >= 768 && windowWidth < 1024) {
+            insure_image.src = "./images/portfolio/tablet/image-portfolio-insure.jpg";
+            console.log(`insure-image src: ${insure_image.src}`);
+        }
+        else if (windowWidth >= 1024) {
+            insure_image.src = "./images/portfolio/desktop/image-portfolio-insure.jpg"; 
+            console.log(`insure-image src: ${insure_image.src}`);
+        } else {
+            insure_image.src = " ./images/portfolio/mobile/image-portfolio-insure.jpg";
+            console.log(`insure-image src: ${insure_image.src}`);
+        }
+    }
+
+    replaceInsure();
+    window.addEventListener('resize', replaceInsure);
+
+
+    /*  ----------------------------
+        "BOOKMARK" PROJECT IMAGE 
+        ---------------------------- */
+    var bookmark_image = document.querySelector(".bookmark");
+    function replaceBookmark() {
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        
+        if(windowWidth >= 768 && windowWidth < 1024) {
+            bookmark_image.src = "./images/portfolio/tablet/image-portfolio-bookmark.jpg";
+            console.log(`bookmark-image src: ${bookmark_image.src}`);
+        }
+        else if (windowWidth >= 1024) {
+            bookmark_image.src = "./images/portfolio/desktop/image-portfolio-bookmark.jpg"; 
+            console.log(`bookmark-image src: ${bookmark_image.src}`);
+        } else {
+            bookmark_image.src = " ./images/portfolio/mobile/image-portfolio-bookmark.jpg";
+            console.log(`bookmark-image src: ${bookmark_image.src}`);
+        }
+    }
+
+    replaceBookmark();
+    window.addEventListener('resize', replaceBookmark);
+
+});
     
 
 
